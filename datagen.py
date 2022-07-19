8f943335a0201fcadb03d30d62d924913ea71203
Nihar Kashyap <nihar1kashyap7@gmail.com>
2022-07-20 01:47:27 +0530

    Insert statement added

--- datagen.py
+++ datagen.py
@@ -22,8 +22,9 @@ args = parser.parse_args()
 for i in range(int(args.count)):
     profile = faker.profile()
     with open("data/" + profile['username'] + '.json', 'a') as prof:
+        profile['website'] = [faker.url() for i in range(faker.random_int(min=1, max=5))]
         del profile['current_location']
         profile['birthdate']=datetime.strftime(profile['birthdate'], '%Y-%m-%d')
         json.dump(profile, prof, indent=4)
         prof.close()
-        upload_file("data/" + profile['username'] + '.json')
+        # upload_file("data/" + profile['username'] + '.json')
